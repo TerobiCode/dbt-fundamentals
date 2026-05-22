aa81ad7d5fe4075ac7fcfe81bc6e097211307353
Sandra Terobi Grosse <sandra.grosse@hyperisland.se>
2026-05-22 09:57:44 +0000

    added data tests

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,9 +1,9 @@
 with customers as (
-    select * from {{ source('jaffle_shop', 'customers') }}
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 ),
 
 orders as (
-    select * from {{ source('jaffle_shop', 'orders') }}
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 ),
 
 customer_orders as (
@@ -12,7 +12,6 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
-        sum(amount) as lifetime_value
     from orders
 
     group by 1
@@ -28,8 +27,6 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_orders.lifetime_value
-
     from customers
     left join customer_orders using (customer_id)
 )
