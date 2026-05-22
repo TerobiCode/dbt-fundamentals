f0fa3cb18abeede5abc34523687875941254bc65
Sandra Terobi Grosse <sandra.grosse@hyperisland.se>
2026-05-22 08:58:05 +0000

    generated new staging models for customers orders and payment

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,9 +1,8 @@
 with customers as (
-    select * from {{ ref('stg_jaffel_shop__customers') }}
-),
+    select * from {{ source('jaffle_shop', 'customers') }}
 
 orders as (
-    select * from {{ ref('fct_orders') }}
+    select * from {{ source('jaffle_shop', 'orders') }}
 ),
 
 customer_orders as (
